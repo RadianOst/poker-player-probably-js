a5b05cf06b595859f656b90a3504ba5adcff9592
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-10-08 11:52:02 +0200

    refactor

--- Player.js
+++ Player.js
@@ -6,29 +6,25 @@ class Player {
   static betRequest(gameState, bet) {
 
     let minimum_raise = gameState.minimum_raise;
-    
-    
+    let our_player = gameState.players[gameState.in_action];
+    let our_hand = our_player.hole_cards;
     let common_cards = gameState.community_cards;
     let output = minimum_raise;
 
+    console.log("----------our player-------------");
+    console.log(our_player);
+
     try {
-      let our_player = gameState.players[gameState.in_action];
-      let our_hand = our_player.hole_cards;
-
-      console.log("----------in_action-------------");
-      console.log(our_player);
-
+    
       output = gameState.current_buy_in - our_player.bet + minimum_raise;
-      console.log("----------in counting-------------");
-      console.log(output);
 
     } catch (e) {
       console.log("---------error----------");
       console.log(e);
     }
     
-    console.log("---------only minimum raise-----------");
-    console.log(minimum_raise + "is? " + output);
+    console.log("---------bet-----------");
+    console.log(output);
     bet(output);
   }
 
