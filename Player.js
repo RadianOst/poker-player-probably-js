94234c7d38f00789f37763f3585ab8e6775e7e4c
adrianOstromecki <bloodrian@gmail.com>
2018-10-08 11:38:08 +0200

    add variables with our cards and community cards

--- Player.js
+++ Player.js
@@ -6,13 +6,19 @@ class Player {
   static betRequest(gameState, bet) {
 
     let minimum_raise = gameState.minimum_raise;
+    let our_player = gameState.players[gameState.in_action];
+    let our_hand = our_player.hole_cards;
+    let common_cards = gameState.community_cards;
+    
     console.log("---------------------------");
     console.log(minimum_raise);
     bet(minimum_raise+1);
   }
 
   static showdown(gameState) {
+      
   }
+   
 }
 
 module.exports = Player;
